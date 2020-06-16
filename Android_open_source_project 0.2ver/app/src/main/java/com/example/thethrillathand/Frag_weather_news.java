9b9cb54eb54d9948fa662a5be8a94d1eff22097c
seunghye0627 <mutanxx@naver.com>
2020-06-16 09:41:02 +0900

    0.2ver

---
+++ Android_open_source_project 0.2ver/app/src/main/java/com/example/thethrillathand/Frag_weather_news.java
@@ -0,0 +1,30 @@
+package com.example.thethrillathand;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag_weather_news extends Fragment {
+
+    Frag_weather_news() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_weather_news,container,false);
+
+        return v;
+    }
+}
